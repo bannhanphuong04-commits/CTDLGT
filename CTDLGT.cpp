b6022325f894c8bd9afcc784fa2423c4a01921b2
bannhanphuong04-commits <bannhanphuong04@gmail.com>
2025-10-09 20:43:03 +0700

    Update CTDLGT.cpp

--- CTDLGT.cpp
+++ CTDLGT.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main() {
-    int a = 5, b = 0;
-    int c = a / b;  // Lỗi chia cho 0
+    int a = 5, b = 0;int c = 0;
+    if (b!=0)
+     c = a / b;  
+    else cout<< "Loi"<< endl;
     cout << "Kết quả: " << c << endl; 
     return 0;
 }
