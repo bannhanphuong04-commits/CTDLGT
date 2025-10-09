de1efe0c16135692944fbbea27db8abeff77580a
Duong Anh <Anh.DT2413963@sis.hust.edu.vn>
2025-10-09 20:40:36 +0700

    them code debug loi chia cho 0

--- CTDLGT.cpp
+++ CTDLGT.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a = 5, b = 0;
     int c = a / b;  // Lỗi chia cho 0
-    cout << "Kết quả: " << c << endl;
+    cout << "Kết quả: " << c << endl; 
     return 0;
 }
